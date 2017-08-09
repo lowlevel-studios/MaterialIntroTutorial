12d0dc1899360f7eba4f42831dc449c34d60311b
Miguel Botón <mboton@lowlevel-studios.com>
2017-08-09 17:01:44 +0200

    Use API SDK 26. Updated support library. Migrated to Glide 4.x

--- materialhelptutorial/src/main/java/za/co/riggaroo/materialhelptutorial/MaterialTutorialFragment.java
+++ materialhelptutorial/src/main/java/za/co/riggaroo/materialhelptutorial/MaterialTutorialFragment.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 /**
  * @author rebeccafranks
@@ -64,13 +65,25 @@ public class MaterialTutorialFragment extends Fragment {
             textViewSubTitle.setText(tutorialItem.getSubTitleTextRes());
         }
         if (tutorialItem.getBackgroundImageRes() != -1) {
-            Glide.with(this).load(tutorialItem.getBackgroundImageRes()).dontAnimate().into(imageViewBack);
+            Glide.with(this)
+                    .load(tutorialItem.getBackgroundImageRes())
+                    .apply(new RequestOptions().dontAnimate())
+                    .into(imageViewBack);
         }
         if (tutorialItem.getForegroundImageRes() != -1 && !tutorialItem.isGif()) {
-            Glide.with(this).load(tutorialItem.getForegroundImageRes()).dontAnimate().into(imageViewFront);
+            Glide.with(this)
+                    .load(tutorialItem.getForegroundImageRes())
+                    .apply(new RequestOptions().dontAnimate())
+                    .into(imageViewFront);
         }
         if (tutorialItem.getForegroundImageRes() != -1 && tutorialItem.isGif()){
-            Glide.with(this).load(tutorialItem.getForegroundImageRes()).asGif().diskCacheStrategy(DiskCacheStrategy.SOURCE).dontAnimate().into(imageViewFront);
+            Glide.with(this)
+                    .asGif()
+                    .load(tutorialItem.getForegroundImageRes())
+                    .apply(new RequestOptions()
+                            .diskCacheStrategy(DiskCacheStrategy.DATA)
+                            .dontAnimate())
+                    .into(imageViewFront);
         }
         return v;
     }
